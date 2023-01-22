270f4e5a6e6ffd8f0ae686d0b9bd1f92e6f52f07
hwetsman <howard@tocdr.com>
2023-01-22 09:41:15 -0600

    Code: initial function to search for map ref

--- read_lit.py
+++ read_lit.py
@@ -7,7 +7,7 @@ import os
 import re
 
 
-def pdf_to_txt(pdf_file, txt_file, password=None):
+def pdf_to_txt(pdf_file, password=None):  # txt_file
     # Open the PDF file
     with open(pdf_file, 'rb') as f:
         # Create resource manager
@@ -29,9 +29,10 @@ def pdf_to_txt(pdf_file, txt_file, password=None):
     # close open handles
     device.close()
     outfp.close()
+    return text
     # Write the text to a text file
-    with open(txt_file, 'w') as f:
-        f.write(text)
+    # with open(txt_file, 'w') as f:
+    #     f.write(text)
 
 
 def return_pages(text):
@@ -58,17 +59,42 @@ def return_pages(text):
     return pages
 
 
+def get_OIG_ref(text):
+    map_ref = r"(?:m\.?a\.?p|M\.?A\.?P|map|Map|MAP|r\.?e\.?f|R\.?E\.?F|ref|Ref|REF)[\s,](\d{3,4}[, ]){2}"
+    numbers = r"(\d{3,4}[, ]){2}"
+    if re.search(map_ref, text):
+        print('Map Ref is found')
+    else:
+        print('Map Ref is not found')
+    if re.search(numbers, text):
+        print('Numbers matching OIG pattern are found')
+    else:
+        print('Numbers matching OIG pattern are not found')
+
+
 pdf_file = 'The_1974_excavation_of_Hayonim_terrace_Israel_A_br.pdf'
 txt_file = 'The_1974_excavation_of_Hayonim_terrace_Israel_A_br.txt'
-pdf_to_txt(pdf_file, txt_file)
-with open(txt_file, 'r') as f:
-    text = f.read()
-print(text)
+# pdf_file = 'Adler_2017_The Decline.pdf'
+# txt_file = 'Adler_2017_The Decline.txt'
+# pdf_file = 'ExcavationReportTombNo1.pdf'
+# txt_file = 'ExcavationReportTombNo1.txt'
+papers = ['The_1974_excavation_of_Hayonim_terrace_Israel_A_br.pdf', 'ExcavationReportTombNo1.pdf']
+for paper in papers:
+    print('\n', paper)
+    text = pdf_to_txt(paper, password=None)
+    # print(text[0:200])
+    get_OIG_ref(text)
+# pdf_to_txt(pdf_file, txt_file)
+# with open(txt_file, 'r') as f:
+#     text = f.read()
+# print(text)
+#
+# pages = return_pages(text)
+# print(len(pages))
+# for page in pages:
+#     print(pages, '\n\n\n\n')
+#
+# new_page = '\x0c'
+# new_line = '\n'
 
-pages = return_pages(text)
-print(len(pages))
-for page in pages:
-    print(pages, '\n\n\n\n')
-
-new_page = '\x0c'
-new_line = '\n'
+# get_OIG_ref(text)
